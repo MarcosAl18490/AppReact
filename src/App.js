bb37d36549d5aae92057b158e28e4ead57965525
marcosal <marcos90.alonso90@gmail.com>
2022-07-27 20:20:07 -0300

    Creando Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './componentes/NavBar';
 
 function App() {
+
+  let userColor = "green";
+  const styles = {marginTop:"20px", color: userColor};
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprender React¡
-        </a>
-      </header>
+    <div>
+      <h1 className="titulo">Resort "Altos de Simic"</h1>
+      <NavBar/>
+        <p style={styles}>El mejor lugar para sus vacaciones</p>
+        <button onClick={ ()=> alert("Su reserva fue realizada")}>Reserve</button>
     </div>
   );
-}
 
+  }
 export default App;
---
+++ src/componentes/NavBar.js
@@ -0,0 +1,15 @@
+import "./NavBar.css";
+
+function NavBar(){
+    return(
+        
+        <ul className="Menu"> 
+            <a href="http://"><li>Principal</li></a>
+            <a href="http://"><li>Habitaciones</li></a>
+            <a href="http://"><li>Servicios</li></a>
+            <a href="http://"><li>Contacto</li></a>
+        </ul>
+    )
+}
+
+export default NavBar;
